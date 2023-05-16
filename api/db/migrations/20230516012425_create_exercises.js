bdb3cb045e74ba75a171d26fb19fcf32e2dd713a
Jason Flowers <poppyjflow@gmail.com>
2023-05-15 18:34:32 -0700

    partial knex DB scripts

---
+++ api/db/migrations/20230516012425_create_exercises.js
@@ -0,0 +1,17 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('exercises', table => {
+    table.increments('id');
+    table.string('exercise_name');
+    table.integer('group').nullable().references('id').inTable('orgs').onDelete('CASCADE');
+    table.integer('wing').nullable().references('id').inTable('orgs').onDelete('CASCADE');
+    table.integer('majcom').nullable().references('id').inTable('orgs').onDelete('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('orgs');
+};
