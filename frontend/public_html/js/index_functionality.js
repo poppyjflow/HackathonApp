f3009f081fe9d226866d579a9125fb8000ee2e09
Braxton Little <braxtonlittle1121@gmail.com>
2023-05-16 15:58:50 -0700

    Working frontend communication with server

---
+++ frontend/public_html/js/index_functionality.js
@@ -0,0 +1,40 @@
+function registerHandlers() {
+  document.getElementById("LoginButton").addEventListener("click", login);
+}
+
+function login() {
+  console.log("Login button pressed");
+  var username = document.getElementById("UserText");
+  console.log("Username: " + username);
+  var password = document.getElementById("PasswordText");
+  if (username.innerText === "" || password.innerText === "") {
+    alert("Please enter a valid username or password");
+    return;
+  }
+  //TODO: confirm user and password match.
+  var data = {
+    username: username,
+    password: password,
+  };
+
+  fetch("http://127.0.0.1:5000/login", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      res.text().then((text) => {
+        console.log("Returned text: " + text);
+      });
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+  //window.location.href = "http://localhost:3000/home.html";
+  //document.cookie = "username=" + username;
+}
+
+document.addEventListener("DOMContentLoaded", registerHandlers);
