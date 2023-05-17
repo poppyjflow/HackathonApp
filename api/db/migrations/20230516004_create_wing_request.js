93077c3a789a38b18000df8c8fa8f762df2938f4
Jason Flowers <poppyjflow@gmail.com>
2023-05-16 19:55:33 -0700

    more acft tbl seeding

--- api/db/migrations/20230516004_create_wing_request.js
+++ api/db/migrations/20230516004_create_wing_request.js
@@ -1,6 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable('wing_request', table => {
     table.increments('id');
+    table.integer('exercises_id').references('id').inTable('exercises').onDelete('CASCADE');
     table.string('unit_name');
     table.string('tdy_from').nullable();
     table.string('tdy_to').nullable();
@@ -8,9 +9,13 @@ exports.up = function (knex) {
     table.integer('days_qty').nullable();
     table.integer('personnel_qty').nullable();
     table.string('acft_type').nullable();
-    table.string('lodging_type').nullable();
-    table.integer('per_diem').nullable();
-    table.boolean('meals_provided').defaultTo(false);
+    table.integer('acft_qty').nullable();
+    table.integer('lodging_qty_gov').nullable();
+    table.integer('lodging_qty_comm').nullable();
+    table.integer('lodging_qty_field').nullable();
+    table.boolean('meals_provided_gov').nullable();
+    table.boolean('meals_provided_comm').nullable();
+    table.boolean('meals_provided_field').nullable();
   });
 };
 
