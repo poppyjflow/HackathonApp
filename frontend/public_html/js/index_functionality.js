786a9df964dbe954d96bcce7c5539f3c0106c288
Braxton Little <braxtonlittle1121@gmail.com>
2023-05-17 10:43:32 -0700

    Added comments to index.js and redesigned login screen

--- frontend/public_html/js/index_functionality.js
+++ frontend/public_html/js/index_functionality.js
@@ -1,3 +1,6 @@
+//Global variables to be used throughout the page
+var isAdmin = false;
+
 // When the content of the page is loaded, add functionality to all
 // necessary items
 document.addEventListener("DOMContentLoaded", registerHandlers);
@@ -13,6 +16,66 @@ function registerHandlers() {
   document
     .getElementById("signInText")
     .addEventListener("click", switchToSignInScreen);
+  document
+    .getElementById("CreateAccountButton")
+    .addEventListener("click", createAccount);
+
+  // Whenever Admin toggle button is pressed, change isAdmin value
+  document.getElementById("adminToggle").addEventListener("click", () => {
+    isAdmin = !isAdmin;
+  });
+}
+
+/*
+ * Called when the create account button is pressed. Grabs the text input values
+ * and throws error if they're empty. Otherwise, salt and hash the passord, grab
+ * the admin boolena value and store the information in the database using fetch
+ * call. If response is successful, add username and access status to cookies
+ * to be used throughout all pages on the website and change the page to
+ * home.html
+ */
+function createAccount() {
+  var username = document.getElementById("CreateUserText");
+  var password = document.getElementById("CreatePasswordText");
+  if (username.value === "" || password.value === "") {
+    alert("Please enter a valid username or password");
+    return;
+  }
+
+  var hashedPassword = hashPassword(password);
+  var createUserBody = {
+    username: username,
+    password: hashedPassword,
+    access: isAdmin ? "Admin" : "User",
+  };
+  fetch("http://127.0.0.1:5000/createuser", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(createUserBody),
+  })
+    .then((res) => {
+      res.json().then((text) => {
+        console.log(text);
+        //if (text.response === "success") {
+        document.cookie += "username=" + text.username + ";";
+        document.cookie += "access=" + text.access + ";";
+        window.location.href = "http://localhost:3000/home.html";
+        return;
+        //}
+        alert("Invalid username/password combination");
+      });
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+}
+
+function hashPassword(password) {
+  //TODO: Hash password
+  return password;
 }
 
 /*
@@ -50,27 +113,28 @@ function login() {
     alert("Please enter a valid username or password");
     return;
   }
-  var data = {
+
+  var hashedPassword = hashPassword(password);
+  var loginBody = {
     username: username,
-    password: password,
+    password: hashedPassword,
   };
-  //TODO: Hash password
   fetch("http://127.0.0.1:5000/login", {
     method: "POST",
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify(loginBody),
   })
     .then((res) => {
       res.json().then((text) => {
         console.log(text);
         //if (text.response === "success") {
-          document.cookie += "username=" + text.username + ";";
-          document.cookie += "access=" + text.access + ";";
-          window.location.href = "http://localhost:3000/home.html";
-          return;
+        document.cookie += "username=" + text.username + ";";
+        document.cookie += "access=" + text.access + ";";
+        window.location.href = "http://localhost:3000/home.html";
+        return;
         //}
         alert("Invalid username/password combination");
       });
