0fd44c37e19d2b96378952e67d749065dc039d97
Kayla Bennett <mbennett3@email.arizona.edu>
2023-05-17 11:55:33 -0700

    added more resources, airframe table WIP

--- api/api_main.py
+++ api/api_main.py
@@ -56,6 +56,7 @@ class AircraftRef(Resource):
         parser.add_argument('year', required=False)
         parser.add_argument('num', required=False)
 
+        n_acft_offset = 2 #row containing price is 2 + (n_aircraft)
         args = parser.parse_args()
         print(args)
 
@@ -64,12 +65,13 @@ class AircraftRef(Resource):
         if args['airframe'] != None:
             query += watoken + 'airframe = '+ args['airframe'] 
             watoken = ' and '
-        if args['year'] != None:
+        if args['fiscal_year'] != None:
             query += watoken + 'year = ' + args['year']
             watoken = ' and '
-        if args['num'] != None:
-            query += watoken + 'num = ' + args['num']
-            watoken = ' and '
+        #pretty sure this isn't needed
+        #if args['num'] != None:
+        #    query += watoken + 'num = ' + args['num'] + 'acft'
+        #    watoken = ' and '
 
 
         query += ';'
@@ -77,26 +79,71 @@ class AircraftRef(Resource):
         cursor = connect_info.conn_handle.cursor()
         cursor.execute(query)
         data = cursor.fetchall()
+        #filter data for a specific number of aircraft
+        if args['num'] != None:
+            pass
+
 
         #No data found
         if(len(data)) == 0:
             msg = jsonify({"rows":"0"})
             return msg
-        
 
+        msg_dict = {}
+        nrow = 0
+        for row in data:
+            msg_dict[str(nrow)] = str(row)
+            nrow += 1
+        msg_dict['rows'] = str(nrow)
+        print(msg_dict)
+        
+    #Edit the aircraft reference
     #only PACAF sessions
     def post(self):
 
         pass
 
-class Exercises(Resource):
 
-    pass
+#Send and get exercise wing requests
+class WingRequest(Resource):
+
+    #See wing requests for an exercise
+    def get(self):
+        pass
+
+    #Submit a wing request for an exercise
+    def post(self):
+        pass
+
+#Get info from per diem chart
+class PerDiem(Resource):
+    def get(self):
+        pass
+
+#TODO:
+    #submit an exercise
+    #view an exercise
+class Exercises(Resource):
+    
+    #Get exercises from the exercise table
+    def get(self):
+        pass
+    
+    #Insert an exercise into the exercise table
+    def post(self):
+
+        parser = reqparse.RequestParser()
+        parser.add_argument("", required=True)
+        parser.add_argument("", required=True)
+        parser.add_argument("", required=True)
+
+        pass
 
 #Login to site
 #Required args:
 #   username (email)
 #   password (hashed password)
+#TODO: return username+rank
 class Login(Resource):
 
     #Attempt to login
@@ -133,15 +180,21 @@ class Login(Resource):
             msg = jsonify({"result":"failure"})
             #make data to return call failure 
 
-        print(msg)
         msg.headers['Access-Control-Allow-Origin']='*'
         msg.headers['Access-Control-Request-Method']='POST, GET, OPTIONS'
         msg.headers['Access-Control-Request-Headers']="Content-Type"
         return msg
 
+
+
+
+
 api.add_resource(Login, "/login")
 api.add_resource(AircraftRef, "/aircraft_reference")
 api.add_resource(Exercises, "/exercises")
+api.add_resource(WingRequest, "/wingrequest")
+api.add_resource(PerDiem, "/perdiem")
+
 if __name__ == '__main__':
     connect_info = Connection(apikeys)
     app.run()
