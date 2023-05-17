ac79465fec32010700f70ee5da86361a721a2c35
Braxton Little <braxtonlittle1121@gmail.com>
2023-05-16 23:46:47 -0700

    Added comments to home and index js files

--- frontend/public_html/js/home_functionality.js
+++ frontend/public_html/js/home_functionality.js
@@ -1,3 +1,10 @@
+// When the content of the page is loaded, add functionality to all
+// necessary items
+document.addEventListener("DOMContentLoaded", registerHandlers);
+
+/*
+ * Adds event listeners to various HMTL elements using helper functions
+ */
 function registerHandlers() {
   document.getElementById("LogoutButton").addEventListener("click", logout);
   /*document.getElementById("NewExerciseButton").addEventListener("click", newEx);
@@ -5,11 +12,19 @@ function registerHandlers() {
     .getElementById("StatusButton")
     .addEventListener("click", changeStatus);
   document.getElementById("NewACTButton").addEventListener("click", newACT);*/
-  document.getElementById("exerciseContainer").addEventListener("click", viewEx);
+  document
+    .getElementById("exerciseContainer")
+    .addEventListener("click", viewEx);
 }
 
+/*
+ * Logs the user out by clearing the cookies which include the
+ * the currently logged in user's username and access status. Once
+ * the cookies are reset, redirect the user back to the login page
+ */
 function logout() {
   console.log("Logging user out...");
+  document.cookie = "";
   window.location.href = "http://127.0.0.1:3000/index.html";
 }
 
@@ -53,5 +68,3 @@ function getCookie(cName) {
   });
   return res;
 }
-
-document.addEventListener("DOMContentLoaded", registerHandlers);
--- frontend/public_html/js/index_functionality.js
+++ frontend/public_html/js/index_functionality.js
@@ -1,7 +1,48 @@
+// When the content of the page is loaded, add functionality to all
+// necessary items
+document.addEventListener("DOMContentLoaded", registerHandlers);
+
+/*
+ * Adds event listeners to various HMTL elements using helper functions
+ */
 function registerHandlers() {
   document.getElementById("LoginButton").addEventListener("click", login);
+  document
+    .getElementById("createAccountText")
+    .addEventListener("click", switchToCreateScreen);
+  document
+    .getElementById("signInText")
+    .addEventListener("click", switchToSignInScreen);
 }
 
+/*
+ * Manually changes the zIndex of the Login window to be in front of
+ * the create account window by giving the login window a higher zIndex
+ * vaue
+ */
+function switchToSignInScreen() {
+  document.getElementById("createAccountContainer").style.zIndex = -1;
+  document.getElementById("loginContainer").style.zIndex = 1;
+}
+
+/*
+ * Manually changes the zIndex of the Creat Account window to be in front of
+ * the login window by giving the Create Account window a higher zIndex
+ * vaue
+ */
+function switchToCreateScreen() {
+  document.getElementById("createAccountContainer").style.zIndex = 1;
+  document.getElementById("loginContainer").style.zIndex = -1;
+}
+
+/*
+ * Called when the login button is pressed. Grabs the text input values
+ * and throws error if they're empty. Otherwise, salt and hash the passord
+ * and attempt to login the user by validating it with database using fetch
+ * call. If response is successful, add username and access status to cookies
+ * to be used throughout all pages on the website and change the page to
+ * home.html
+ */
 function login() {
   var username = document.getElementById("UserText");
   var password = document.getElementById("PasswordText");
@@ -37,8 +78,4 @@ function login() {
     .catch((error) => {
       alert(error.message);
     });
-  //window.location.href = "http://localhost:3000/home.html";
-  //document.cookie += "username=" + username;
 }
-
-document.addEventListener("DOMContentLoaded", registerHandlers);
